1c80f0c4da3091c2c5ceda22b112a0522afb0b90
Martin Pettersson <kontakt@martinpettersson.se>
2018-07-15 22:00:33 +0200

    Started on heap.

--- Hello.java
+++ Hello.java
@@ -2,54 +2,53 @@ package test;
 
 public class Hello {
 	public static void main(String[] args) {
-		System.out.println("Hello World");
-		int[] a = {19519,181,711,51,1,15,18,11,7,67,1};
-		printArray(a);
+		int[] a = {18181,78,11,51,57,12,1,81,516,18,291,51,6,15};
 		Hello o = new Hello();
-		o.heapsort(a);
 		printArray(a);
+o.heapsort(a);
+printArray(a);
 	}
-	
+
 	public static void printArray(int[] a) {
 		for (int e : a) {
 			System.out.print(e + " ");
 		}
 		System.out.println();
 	}
-	
+
 	public void heapsort(int[] a) {
 		int n = a.length;
 		
 		for (int i = n/2-1; i >= 0; i--) {
-			heapify(a,n,i);
+			heapify(a, n, i);
 		}
-		
-		for (int i = n-1; i >= 0; i--) {
+
+		for (int i = n - 1; i >= 0; i--) {
 			int tmp = a[0];
 			a[0] = a[i];
 			a[i] = tmp;
 			heapify(a,i,0);
 		}
 	}
-	
+
 	public void heapify(int[] a, int n, int i) {
 		int hi = i;
 		int l = 2*i+1;
 		int r = 2*i+2;
-		
+
 		if (l < n && a[l] > a[hi]) {
 			hi = l;
 		}
-		
+
 		if (r < n && a[r] > a[hi]) {
 			hi = r;
 		}
-		
-		if (i != hi) {
+
+		if (hi != i) {
 			int tmp = a[i];
 			a[i] = a[hi];
 			a[hi] = tmp;
-			heapify(a,n,hi);
+			heapify(a, n, hi);
 		}
 	}
 }
